ecd502463384e7b89018b34f48b8ab89e045360e
Grupa1 <grupa1@administrators-iMac-5.local>
2018-07-12 20:52:40 +0200

    a

--- app/src/main/java/com/example/grupa1/vremenskaprognoza/WeatherServise.java
+++ app/src/main/java/com/example/grupa1/vremenskaprognoza/WeatherServise.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface WeatherServise {
-    @GET("/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22")
-    Call<Example> getWeather();
+    @GET("/data/2.5/weather?appid=57a1eb16a020293ee73630799720c1e5")
+    Call<Example> getWeather(@Query("q") String city);
 }
