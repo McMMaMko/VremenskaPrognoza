efea435ea13018b59ec8a860a143f0f289ebcaef
McMMaMko <41018161+McMMaMko@users.noreply.github.com>
2018-07-13 20:38:34 +0200

    prva kucna promjena

--- app/src/main/java/com/example/grupa1/vremenskaprognoza/Example.java
+++ app/src/main/java/com/example/grupa1/vremenskaprognoza/Example.java
@@ -85,7 +85,9 @@ public class Example {
 
 
         public String getTemp() {
-            return String.format("%.02f", temp-273.15);
+            temp =- 273.15;
+
+            return String.format("%.02f", temp);
         }
     }
     }
