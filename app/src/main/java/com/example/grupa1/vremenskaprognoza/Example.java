ca117c0e3c738210116ac94e75adf5a403459d4a
McMMaMko <41018161+McMMaMko@users.noreply.github.com>
2018-07-13 21:39:15 +0200

    temperature

--- app/src/main/java/com/example/grupa1/vremenskaprognoza/Example.java
+++ app/src/main/java/com/example/grupa1/vremenskaprognoza/Example.java
@@ -85,9 +85,10 @@ public class Example {
 
 
         public String getTemp() {
-            temp =- 273.15;
+            temp -= 273.15;
 
-            return String.format("%.02f", temp);
+
+            return String.format("%.0f", temp)+"°";
         }
     }
     }
